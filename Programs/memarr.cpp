bbd58cf77a19133ffdf4243658e3ebd68dc39910
Strangerfuture <strangerfuture@proton.me>
2025-09-06 20:40:37 +0545

    made some change

--- Programs/memarr.cpp
+++ Programs/memarr.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
 using namespace std;
+
 int main()
 {
+    int n;
+    cout << "Enter the size of array : ";
+    cin >> n;
+    int *arr = new int[n]; // dynamically allocating the memeory size of an int * n
+    for (auto i = 0; i < n; i++)
+    {
+        cout << "Enter the " << i + 1 << " Number : ";
+        cin >> arr[i]; // dereference operator
+    }
+
+    for (auto i = 0; i < n; i++)
+    {
+        cout << "The value is : " << arr[i] << endl;
+        cout << "The address is : " << &arr[i] << endl;
+    }
+
+    delete[] arr; // freeing the allocated space
+    arr = nullptr;
     return 0;
-    
 }
